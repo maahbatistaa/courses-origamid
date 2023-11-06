eda31e6696770844da72527bd7da057a31879073
Mariana Batista <mariana-batista99@hotmail.com>
2023-11-06 13:16:40 -0300

    :construction:

---
+++ JavaScript Completo ES6+/numeros-operadores/script-aula.js
@@ -0,0 +1,23 @@
+var idade = 25;
+var time = null;
+console.log(typeof time);
+
+var nome = 'Mariana';
+var sobrenome = 'Batista';
+var nomeCompleto = nome + ' ' + sobrenome;
+console.log(nomeCompleto);
+
+// var gols = 1000;
+// var frase = 'Romario fez ' + gols + ' gols';
+// console.log(frase);
+
+var ano = '2018';
+var mes = 8;
+console.log(ano + mes);
+
+var frase1 = 'Esse é o "melhor" jogo';
+console.log(frase1);
+
+var gols = 1000;
+var frase2 = `Romario fez ${gols * 2} gols`;
+console.log(frase2);
---
+++ JavaScript Completo ES6+/variaveis/script-aula.js
@@ -0,0 +1,16 @@
+var nome = 'Mariana';
+var idade = 25;
+var possuiFaculdade = true;
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+console.log(totalPreco);
+
+var sobrenome = 'Batista',
+  cidade = 'Curitiba';
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
