7c838497972359f5a32bbd7e2c43201efdf93667
Mariana Batista <mariana-batista99@hotmail.com>
2023-11-17 15:30:11 -0300

    :construction:

---
+++ JavaScript Completo ES6+/tudo-e-objeto/script.js
@@ -0,0 +1,17 @@
+// nomeie 3 propriedades ou métodos de strings
+var nome = 'Mariana'
+nome.charAt()
+nome.length
+nome.slice()
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+var btn = document.querySelector('.btn')
+
+btn.addEventListener()
+btn.appendChild()
+btn.innerHTML()
+btn.id
+btn.outerHTML
+
+// busque na web um objeto (método) capaz de interagir com o clipboard,
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
