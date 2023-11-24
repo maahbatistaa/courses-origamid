069aa394ce64ee45ec50c1f2d38d4827b59e7d43
Mariana Batista <mariana-batista99@hotmail.com>
2023-11-24 11:27:28 -0300

    :sparkles: javascript para iniciantes

---
+++ JavaScript Completo ES6+/javacript-para-iniciantes/atribuicao-e-ternario/script.js
@@ -0,0 +1,17 @@
+// Some 500 ao valor de scroll abaixo,
+// atribuindo o novo valor a scroll
+var click = 1000;
+click += 500
+console.log(click)
+
+// Atribua true para a variável darCredito,
+// caso o cliente possua carro e casa.
+// E false caso o contrário.
+var possuiCarro = true;
+var possuiCasa = true;
+var darCredito;
+
+darCredito = (possuiCarro && possuiCasa) ? 'Dar credito' : 'Negar credito'
+console.log(darCredito)
+
+
