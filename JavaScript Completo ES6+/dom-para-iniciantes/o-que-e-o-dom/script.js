22069045d2252bc99f9a215174ea1e5383fc8a7b
Mariana Batista <mariana-batista99@hotmail.com>
2023-11-24 14:45:48 -0300

    :construction:

---
+++ JavaScript Completo ES6+/dom-para-iniciantes/o-que-e-o-dom/script.js
@@ -0,0 +1,12 @@
+// Retorne o url da página atual utilizando o objeto window
+var href = window.location.href
+console.log(href)
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const elementoAtivo = document.querySelector('.ativo')
+console.log(elementoAtivo)
+
+// Retorne a linguagem do navegador
+
+// Retorne a largura da janela
