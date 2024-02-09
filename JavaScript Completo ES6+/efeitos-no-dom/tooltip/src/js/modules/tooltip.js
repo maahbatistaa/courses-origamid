297f706cdd0a92d6bc373ab55a5d9c2de405524b
Mariana Batista <mariana-batista99@hotmail.com>
2024-02-09 14:05:16 -0300

    :construction:

---
+++ JavaScript Completo ES6+/efeitos-no-dom/tooltip/src/js/modules/tooltip.js
@@ -0,0 +1,13 @@
+export default function initTooltip(){
+
+}
+
+const tooltips = document.querySelectorAll('[data-tooltip]')
+
+tooltips.forEach((item) => {
+  item.addEventListener('mouseover', onMouseOver)
+})
+
+function onMouseOver(event){
+  console.log(event)
+}
