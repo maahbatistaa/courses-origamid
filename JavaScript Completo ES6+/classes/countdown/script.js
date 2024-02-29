bf3e127eb8f5ac94e1b3a1c7bb8c04093de828f0
Mariana Batista <mariana-batista99@hotmail.com>
2024-02-29 08:41:34 -0300

    :sparkles: countdown

--- JavaScript Completo ES6+/classes/countdown/script.js
+++ JavaScript Completo ES6+/classes/countdown/script.js
@@ -1,5 +1,6 @@
 import Countdown from './countdown.js';
 
-const diasParaONatal = new Countdown('24 December 2018 23:59:59 GMT-0300');
+const tempoParaONatal = new Countdown('24 December 2024 23:59:59 GMT-0300');
 
-console.log(diasParaONatal)
+console.log(tempoParaONatal.total)
+
