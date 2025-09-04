d2d4f18b80c2f2bbdb2a063c0ef86cead74f3073
Mariana <mariana-batista99@hotmail.com>
2025-09-04 16:56:29 -0300

    types e interface

---
+++ TypeScript para Iniciantes/02-typescript-basico/union-types/script.ts
@@ -0,0 +1,18 @@
+let total: string | number = 200;
+
+function isNumber(value: string | number) {
+  if (typeof value === "number") return true;
+  else return false;
+}
+
+console.log(isNumber("200"));
+
+const button = document.querySelector("button");
+
+function toNumber(value: number | string) {
+  if (typeof value === "number") return value;
+  else if (typeof value === "string") return Number(value);
+  else throw "value deve ser um number | string";
+}
+
+console.log(toNumber("100"));
