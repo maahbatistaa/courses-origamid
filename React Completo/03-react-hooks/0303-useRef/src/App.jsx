c91df483120830ce775d9df5712045f85174dd3c
Mariana <mariana-batista99@hotmail.com>
2026-03-22 12:40:37 -0300

    feat(React): useRef

---
+++ React Completo/03-react-hooks/0303-useRef/src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const App = () => {
+  const [carrinho, setCarrinho] = React.useState(0);
+  const [notificacao, setNotificacao] = React.useState(null);
+  const timeoutRef = React.useRef();
+
+  function handleClick() {
+    setCarrinho(carrinho + 1);
+    setNotificacao("Item adicionado ao carrinho!");
+
+    clearTimeout(timeoutRef.current);
+
+    timeoutRef.current = setTimeout(() => {
+      setNotificacao(null);
+    }, 2000);
+  }
+
+  return (
+    <div>
+      <p>{notificacao}</p>
+      <button onClick={handleClick}>Adicionar carrinho {carrinho}</button>
+    </div>
+  );
+};
+
+export default App;
