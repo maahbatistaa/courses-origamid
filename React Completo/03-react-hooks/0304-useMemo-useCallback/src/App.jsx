76cd88295dabb2caa0e9ff1ebd630689cb394d14
Mariana <mariana-batista99@hotmail.com>
2026-03-28 19:05:02 -0300

    feat(React): useMemo e useCallback

---
+++ React Completo/03-react-hooks/0304-useMemo-useCallback/src/App.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const set1 = new Set();
+const set2 = new Set();
+
+const Produto = () => {
+  const func1 = () => {
+    console.log("Produto 1");
+  };
+
+  const func2 = React.useCallback(() => {
+    console.log("Produto 2");
+  }, []);
+
+  set1.add(func1);
+  set2.add(func2);
+
+  console.log("Set 1:", set1);
+  console.log("Set 2:", set2);
+
+  return (
+    <div>
+      <p onClick={func1}>Produto 1</p>
+      <p onClick={func2}>Produto 2</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const [contar, setContar] = React.useState(0);
+
+  return (
+    <div>
+      <Produto />
+      <button onClick={() => setContar(contar + 1)}>Contar: {contar}</button>
+    </div>
+  );
+};
+
+export default App;
